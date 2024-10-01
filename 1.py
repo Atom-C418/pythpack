8a9a39be59a13e2b310a670e2750012ed3e80de8
Atom <atom.crafard@gmail.com>
2024-10-01 11:15:51 -0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,5 @@
+age = int(input("What is your age? "))
+print("You have been alive for", age*365, "days.")
+print(age/10, "decades.")
+print(age*52, "weeks.")
+print(age*525600, "minutes.")
---
+++ 2.py
@@ -0,0 +1,6 @@
+noun = input("Give me a noun: ")
+adj = input("Give me an adjective: ")
+name = input("Give me a person's name (capitalize it ffs): ")
+
+print(name, "was walking down the road one day when they saw a", noun + ". They thought it was really cool, and picked it up. They took it home back to their", noun, "collection. They had a lot. It was very", adj+". "+name, "loves",noun+"s. Do you?")
+#this is so dumb
